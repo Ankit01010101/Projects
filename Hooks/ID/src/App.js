fce850bd88bbfb5fbeee0ed052e518792a6f4e15
Ankit01010101 <140271139+Ankit01010101@users.noreply.github.com>
2023-09-15 20:11:25 +0530

    this is my project file
    project

---
+++ Hooks/ID/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Restaurant from './component/Basic/Restaurant';
+
+
+let App = () => {
+  return(
+    <div>
+      <Restaurant />
+    </div>
+  )
+}
+export default App;
---
+++ Hooks/ID/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+// import App from './App';
+import UseState from './component/Hooks/useState';
+import UseEffect from './component/Hooks/useEffect';
+import UseReducer from './component/Hooks/useReducer';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <UseEffect />
+  </React.StrictMode>
+);
+
+
+
